7d5aab3de15cc1ffebe3554ad6bfa901fa8e5814
Adam Davidson <adam99davidson@gmail.com>
2022-10-05 21:31:14 +1300

    Add project files.

---
+++ MoviesLibrary/MovieModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesLibrary
+{
+    public class MovieModel
+    {
+        /// <summary>
+        /// The name of the movie.
+        /// </summary>
+        public string MovieName { get; set; }
+
+        /// <summary>
+        /// The director of the movie.
+        /// </summary>
+        public PersonModel MovieDirector { get; set; }
+
+        /// <summary>
+        /// List of stars in the movie.
+        /// </summary>
+        public List<PersonModel> MovieStars { get; set; } = new List<PersonModel>();
+
+        /// <summary>
+        /// Year the movie was released. 
+        /// </summary>
+        public int MovieYear { get; set; }
+
+        /// <summary>
+        /// List of genres of the movie.
+        /// </summary>
+        public List<string> MovieGenre { get; set; } = new List<string>();
+
+        /// <summary>
+        /// Language the movie is in
+        /// </summary>
+        public string MovieLanguage { get; set; }
+
+        /// <summary>
+        /// My rating of the movie out of 10
+        /// </summary>
+        public int MovieRating { get; set; }
+
+    }
+}
---
+++ MoviesLibrary/PersonModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesLibrary
+{
+    public class PersonModel
+    {
+        /// <summary>
+        /// The first name of the person.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the person.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// List of movies this person has stared in.
+        /// </summary>
+        public List<MovieModel> Star { get; set; } = new List<MovieModel>();
+        
+        /// <summary>
+        /// List of movies this person has directed 
+        /// </summary>
+        public List<MovieModel> Director { get; set; } = new List<MovieModel>();
+    }
+}
